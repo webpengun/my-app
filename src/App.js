3b07ad1da52cc886ed1f5f531ba847e3efc5cbc6
webpengun <webpengun@gmail.com>
2025-05-17 16:12:11 +0900

    再度タイトルを変更しました

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>アプリケーション</h1>
+                <h1>gitaaのテスト</h1>
                 <button>テスト</button>
             </header>
         </div>
